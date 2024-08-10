0a3f8367d791f41291ee5f769ff10a9be376a995
debanjan maji <debanjanmaji700@gmail.com>
2024-08-11 00:35:44 +0530

    nothing

--- src/scripts.js
+++ src/scripts.js
@@ -6,3 +6,20 @@ document.addEventListener('DOMContentLoaded', function() {
         navLinks.classList.toggle('active');
     });
 });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
